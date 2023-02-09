d78b0c1f57298977f6bc84c1ea025d240e2f0a2b
mohammad-Alsmadi <60603704+mohammadalsmadi2000@users.noreply.github.com>
2023-02-09 15:19:23 +0300

    Comments to improve readability

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-// alert("sss");
 
-function Employee(employeeID, fullName, department, level, imageURL, salary) {
+//create a constructor to generate an employee object
+
+function Employee(employeeID, fullName, department, level, imageURL) {
     this.employeeID = employeeID;
     this.fullName = fullName;
     this.department = department;
     this.level = level;
     this.imageURL = imageURL;
 }
+//create a prototype function for calculating the salary  
 
 Employee.prototype.salary = function () {
     let max, min, tax = 0.075;
@@ -25,12 +27,12 @@ Employee.prototype.salary = function () {
         } break;
     }
     let salary = Math.floor(Math.random() * (max - min) + min);
-    console.log(`salary : ${salary}`);
+//calculate the net salary where the tax percent is 7.5.
     let salaryAfterTax = salary - salary * tax;
-    console.log(`salaryAfterTax : ${salaryAfterTax}`);
     return salaryAfterTax;
 }
 
+//create a render prototype function
 Employee.prototype.render = function () {
     return document.write(`<table style="border: 1px solid black; border-collapse: collapse;">
                             <tr style="background-color: #8ac5ff;">
@@ -45,7 +47,7 @@ Employee.prototype.render = function () {
                          `)
 }
 
-
+//create employees instances
 let e1 = new Employee(1000, 'Ghazi Samer', 'Administration', 'Senior', "url");
 let e2 = new Employee(1001, 'Lana Ali', 'Finance', 'Senior', "url");
 let e3 = new Employee(1002, 'Tamara Ayoub', 'Marketing', 'Senior', "url");
@@ -54,10 +56,14 @@ let e5 = new Employee(1004, 'Omar Zaid', 'Development', 'Senior', "url");
 let e6 = new Employee(1005, 'Rana Saleh', 'Development', 'Junior', "url");
 let e7 = new Employee(1006, 'Hadi Ahmad', 'Finance', 'Mid-Senior', "url");
 
+//This array was used to control the instances
 let employeeArray = [e1, e2, e3, e4, e5, e6, e7];
 
+//This loop was used to generate 7 renders
 for (let i = 0; i <= 6; i++) {
     employeeArray[i].render();
     document.write("<br>");
 }
-console.log(e1.salary())
+
+
+// The ( console.log) has been removed for the sake of performance
